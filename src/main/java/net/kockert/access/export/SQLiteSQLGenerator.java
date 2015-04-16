ca76846eeb27c09e44a4983285bb62e329f07411
hzpz <mailantimo@gmx.de>
2015-04-16 22:44:55 +0200

    Refactoring

--- src/main/java/net/kockert/access/export/SQLiteSQLGenerator.java
+++ src/main/java/net/kockert/access/export/SQLiteSQLGenerator.java
@@ -20,8 +20,6 @@ public class SQLiteSQLGenerator implements SQLGenerator {
         final StringBuilder stmtBuilder = new StringBuilder();
 
         List<? extends Index.Column> primaryKeyColumns = getPrimaryKeyColumns(table);
-        boolean hasSinglePrimaryKeyColumn = primaryKeyColumns.size() == 1;
-        boolean hasMultiplePrimaryKeyColumns = primaryKeyColumns.size() > 1;
 
         String tableName = table.getName();
         stmtBuilder.append("CREATE TABLE ");
@@ -34,45 +32,9 @@ public class SQLiteSQLGenerator implements SQLGenerator {
 
             stmtBuilder.append(createStringConstant(column.getName()));
             stmtBuilder.append(" ");
-            // see https://www.sqlite.org/datatype3.html
-            switch (column.getType()) {
-                /* Blob */
-                case BINARY:
-                case OLE:
-                    stmtBuilder.append("BLOB");
-                    break;
+            stmtBuilder.append(mapDatatype(column));
 
-                /* Integers */
-                case BOOLEAN:
-                case BYTE:
-                case INT:
-                case LONG:
-                case MONEY:
-                /* SQLite does not have a dedicated type for storing timestamps and the
-                 * default behaviour of the driver is to store the date as Unix Time */
-                case SHORT_DATE_TIME:
-                    stmtBuilder.append("INTEGER");
-                    break;
-
-                /* Floating point */
-                case DOUBLE:
-                case FLOAT:
-                case NUMERIC:
-                    stmtBuilder.append("REAL");
-                    break;
-
-                /* Strings */
-                case TEXT:
-                case GUID:
-                case MEMO:
-                    stmtBuilder.append("TEXT");
-                    break;
-
-                default:
-                    throw new IllegalArgumentException("Unsupported data type: " + column.getType());
-            }
-
-            if (hasSinglePrimaryKeyColumn && primaryKeyColumns.get(0).getColumn().equals(column)) {
+            if (isPrimaryKeyColumn(primaryKeyColumns, column)) {
                 stmtBuilder.append(" PRIMARY KEY");
             }
 
@@ -81,15 +43,9 @@ public class SQLiteSQLGenerator implements SQLGenerator {
             }
         }
 
-        if (hasMultiplePrimaryKeyColumns) {
-            stmtBuilder.append(", PRIMARY KEY(");
-            for (Iterator<? extends Index.Column> iterator = primaryKeyColumns.iterator(); iterator.hasNext(); ) {
-                stmtBuilder.append(iterator.next().getName());
-                if (iterator.hasNext()) {
-                    stmtBuilder.append(", ");
-                }
-            }
-            stmtBuilder.append(")");
+        if (hasMultiplePrimaryKeyColumns(primaryKeyColumns)) {
+            stmtBuilder.append(", ");
+            stmtBuilder.append(createPrimaryKeyTableConstraint(primaryKeyColumns));
         }
 
         for (Relationship relationship : relationships) {
@@ -97,27 +53,135 @@ public class SQLiteSQLGenerator implements SQLGenerator {
                 continue;
             }
 
-            stmtBuilder.append(", FOREIGN KEY(");
-            for (Iterator<Column> iterator = relationship.getToColumns().iterator(); iterator.hasNext(); ) {
-                Column foreignKeyColumn = iterator.next();
-                stmtBuilder.append(foreignKeyColumn.getName());
-                if (iterator.hasNext()) {
-                    stmtBuilder.append(", ");
-                }
-            }
-            stmtBuilder.append(") REFERENCES ");
-            stmtBuilder.append(relationship.getFromTable().getName());
-            stmtBuilder.append("(");
-            for (Iterator<Column> iterator = relationship.getFromColumns().iterator(); iterator.hasNext(); ) {
-                Column referencedColumn = iterator.next();
-                stmtBuilder.append(referencedColumn.getName());
-                if (iterator.hasNext()) {
-                    stmtBuilder.append(", ");
-                }
-            }
-            stmtBuilder.append(")");
+            stmtBuilder.append(", ");
+            stmtBuilder.append(createForeignKeyTableConstraint(relationship));
         }
 
+        stmtBuilder.append(")");
+
+        return stmtBuilder.toString();
+    }
+
+    /**
+     * Maps the type of the given column to an SQLite datatype.
+     *
+     * @param column the column
+     * @return an SQLite datatype
+     * @see <a href="https://www.sqlite.org/datatype3.html">Datatypes in SQLite</a>
+     */
+    private String mapDatatype(Column column) {
+        switch (column.getType()) {
+            /* Blob */
+            case BINARY:
+            case OLE:
+                return "BLOB";
+
+            /* Integers */
+            case BOOLEAN:
+            case BYTE:
+            case INT:
+            case LONG:
+            case MONEY:
+            /* SQLite does not have a dedicated type for storing timestamps and the
+             * default behaviour of the driver is to store the date as Unix Time */
+            case SHORT_DATE_TIME:
+                return "INTEGER";
+
+            /* Floating point */
+            case DOUBLE:
+            case FLOAT:
+            case NUMERIC:
+                return "REAL";
+
+            /* Strings */
+            case TEXT:
+            case GUID:
+            case MEMO:
+                return "TEXT";
+
+            default:
+                throw new IllegalArgumentException("Unsupported data type: " + column.getType());
+        }
+    }
+
+    /**
+     * Returns {@code true} if the primary key consists solely of the given column.
+     *
+     * @param primaryKeyColumns the columns of the primary key
+     * @param column the column
+     * @return {@code true} if the column is the only column of the primary key, otherwise {@code false}
+     */
+    private boolean isPrimaryKeyColumn(List<? extends Index.Column> primaryKeyColumns, Column column) {
+        return primaryKeyColumns.size() == 1 && primaryKeyColumns.get(0).getColumn().equals(column);
+    }
+
+    /**
+     * Returns {@code true} if the primary key consists of more than one column.
+     *
+     * @param primaryKeyColumns the columns of the primary key
+     * @return {@code true} if the primary key consists of more than one column, otherwise {@code false}
+     */
+    private boolean hasMultiplePrimaryKeyColumns(List<? extends Index.Column> primaryKeyColumns) {
+        return primaryKeyColumns.size() > 1;
+    }
+
+    /**
+     * Creates a primary key table constraint for multiple columns, e.g.
+     * <pre>
+     * PRIMARY KEY('column1', 'column2')
+     * </pre>
+     *
+     * @param primaryKeyColumns the columns of the primary key
+     * @return a primary key table contraint
+     * @see <a href="https://www.sqlite.org/syntax/table-constraint.html">SQLite table-contraint</a>
+     */
+    private String createPrimaryKeyTableConstraint(List<? extends Index.Column> primaryKeyColumns) {
+        final StringBuilder stmtBuilder = new StringBuilder();
+
+        stmtBuilder.append("PRIMARY KEY(");
+        for (Iterator<? extends Index.Column> iterator = primaryKeyColumns.iterator(); iterator.hasNext(); ) {
+            stmtBuilder.append(createStringConstant(iterator.next().getName()));
+            if (iterator.hasNext()) {
+                stmtBuilder.append(", ");
+            }
+        }
+        stmtBuilder.append(")");
+
+        return stmtBuilder.toString();
+    }
+
+    /**
+     * Creates a foreign key table constraint, e.g.
+     * <pre>
+     * FOREIGN KEY('otherTableId') REFERENCES otherTable('id')
+     * </pre>
+     *
+     * @param relationship the relationship from the Access database
+     * @return a foreign key constraint
+     * @see <a href="https://www.sqlite.org/syntax/table-constraint.html">SQLite table-contraint</a>
+     * @see <a href="https://www.sqlite.org/syntax/foreign-key-clause.html">SQLite foreign-key-clause</a>
+     */
+    private String createForeignKeyTableConstraint(Relationship relationship) {
+        final StringBuilder stmtBuilder = new StringBuilder();
+
+        stmtBuilder.append("FOREIGN KEY(");
+        for (Iterator<Column> iterator = relationship.getToColumns().iterator(); iterator.hasNext(); ) {
+            Column foreignKeyColumn = iterator.next();
+            stmtBuilder.append(createStringConstant(foreignKeyColumn.getName()));
+            if (iterator.hasNext()) {
+                stmtBuilder.append(", ");
+            }
+        }
+        stmtBuilder.append(") REFERENCES ");
+        stmtBuilder.append(relationship.getFromTable().getName());
+        stmtBuilder.append("(");
+        for (Iterator<Column> iterator = relationship.getFromColumns().iterator(); iterator.hasNext(); ) {
+            Column referencedColumn = iterator.next();
+            stmtBuilder.append(createStringConstant(referencedColumn.getName()));
+            if (iterator.hasNext()) {
+                stmtBuilder.append(", ");
+            }
+        }
         stmtBuilder.append(")");
 
         return stmtBuilder.toString();
@@ -145,7 +209,7 @@ public class SQLiteSQLGenerator implements SQLGenerator {
      *
      * @param string the string to create a constant from
      * @return the string as a SQLite string constant
-     * @see <a href="http://www.sqlite.org/lang_expr.html>SQL As Understood By SQLite</a>
+     * @see <a href="http://www.sqlite.org/lang_expr.html">SQL As Understood By SQLite</a>
      */
     private String createStringConstant(String string) {
         return "'" + string.replace("'", "''") + "'";
