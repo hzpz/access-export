69bff580090413eaec03e0276127b41e7d3d057c
hzpz <mailantimo@gmx.de>
2015-04-19 21:22:38 +0200

    Added another test for Main class

--- src/test/java/net/kockert/access/export/MainIT.java
+++ src/test/java/net/kockert/access/export/MainIT.java
@@ -2,6 +2,12 @@ package net.kockert.access.export;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.junit.Assume.assumeTrue;
+
 public class MainIT {
 
     @Test(expected = SystemExitRuntimeException.class)
@@ -26,6 +32,15 @@ public class MainIT {
     public void shouldExitIfSourceFileIsEmpty() {
         Main main = new Main(new TestSystemExitHandler());
         main.run("src/test/resources/empty.mdb", "targetFile");
+    }
+
+    @Test(expected = SystemExitRuntimeException.class)
+    public void shouldExitIfTargetFileIsNotWriteable() throws IOException {
+        File tempDirectory = Files.createTempDirectory("access-export-").toFile();
+        tempDirectory.deleteOnExit();
+        assumeTrue(tempDirectory.setReadOnly());
+        Main main = new Main(new TestSystemExitHandler());
+        main.run("src/test/resources/source.mdb", tempDirectory + "/aTargetFileThatDoesNotExist");
     }
 
     class TestSystemExitHandler implements Main.SystemExitHandler {
